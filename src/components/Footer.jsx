b68291fd897f75cce26ca20155b9141c8058eb45
albaduro <81486792+albaduro@users.noreply.github.com>
2026-06-06 21:02:48 +0200

    Add authors list to the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,13 @@ const Footer = () => {
             <li><a href="#privacidad">Política de Privacidad</a></li>
             <li><a href="#cookies">Política de Cookies</a></li>
         </ul>
+      <ul className="footer-autores">
+            <li><a span="#autores">Lidia Logroño Ramas</a></li>
+            <li><a span="#autores">Alba Duro Cabrera</a></li>
+            <li><a span="#autores">Marco Antonio Fernández Heras</a></li>
+            <li><a span="#autores">Pablo Pérez-Aradros Calvo</a></li>
+            <li><a span="#autores">Francisco José Saiz Ezquerra de Paz</a></li>
+        </ul>
     </footer>
   );
 }
